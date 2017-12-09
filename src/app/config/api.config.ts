d893fb052b9b19ed714b18cdcf62db636edc3cf6
andream16 <andrea.medda.uni@gmail.com>
2017-12-09 14:02:54 +0100

    Fixing gitignore

--- src/app/config/api.config.ts
+++
@@ -1,8 +0,0 @@
-import { environment } from '../../environments/environment';
-
-/** Base **/
-export const apiBaseEndpoint = [['http://', environment.production ? '54.194.199.230' : 'localhost'].join('/'), '8080'].join(':');
-
-/** Items **/
-export const apiItemBaseEndpoint = 'item';
-
