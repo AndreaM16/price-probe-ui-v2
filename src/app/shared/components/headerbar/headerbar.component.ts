c150f19e139d12fd1676fcfd7393a43aebb12870
andream16 <andrea.medda.uni@gmail.com>
2017-12-10 13:25:49 +0100

    Adding header and footer components.

---
+++ src/app/shared/components/headerbar/headerbar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-bar',
+  templateUrl: `./headerbar.component.html`,
+  styleUrls: ['./headerbar.component.scss']
+})
+
+export class HeaderbarComponent {}
