de4ebe3ab15636af46a98695096776bcdad54f4d
andream16 <andrea.medda.uni@gmail.com>
2017-12-16 17:28:33 +0100

    Fixed Item to handle a proper visualization.

--- src/app/shared/components/item/item.component.ts
+++ src/app/shared/components/item/item.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 /** App Models **/
 import { Item } from '../../models/item.model';
@@ -9,11 +10,31 @@ import { Item } from '../../models/item.model';
   styleUrls: ['./item.component.scss']
 })
 
-export class ItemComponent {
+export class ItemComponent implements OnInit {
   @Input() item: Item;
-  constructor() {
-    if ( this.item !== undefined && (!this.item.image || this.item.image.length === 0)) {
-      this.item.image = '../../../../../assets/images/item_not_found.png';
+  notFoundPath = '../../../../assets/images/item_not_found.png';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    if ( this.item !== undefined &&
+         (!this.item.image || this.item.image == null || this.item.image === undefined || this.item.image.length === 0)) {
+        this.item.image = this.notFoundPath;
+    }
+    if ( this.item !== undefined &&
+      (!this.item.title || this.item.title == null || this.item.title === undefined || this.item.title.length === 0)) {
+      this.item.title = 'No title available';
     }
   }
+
+  goToAmazon(item: Item) {
+    if (item !== undefined && item.url && item.url.length) {
+      window.open(item.url);
+    }
+  }
+
+  goToDetails(item: Item) {
+    this.router.navigate(['app/details', item.id]);
+  }
+
 }
