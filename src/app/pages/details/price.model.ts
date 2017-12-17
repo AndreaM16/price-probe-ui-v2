b303aba8c348be9a56b63561e5e70034c3d0b948
andream16 <andrea.medda.uni@gmail.com>
2017-12-17 16:35:19 +0100

    Putting both prices and chart models in the same file.

--- src/app/pages/details/price.model.ts
+++ src/app/pages/details/price.model.ts
@@ -3,11 +3,36 @@ export class PriceResponse {
   prices: PriceEntry[];
   constructor(priceJson: any) {
     this.item = priceJson.item;
-    this.prices = priceJson.prices;
+    this.prices = priceJson.prices.map((price) => new PriceEntry(price));
   }
 }
 
 export class PriceEntry {
   date: string;
   price: number;
+  constructor(priceEntryJson: any) {
+    this.date = priceEntryJson.date;
+    this.price = priceEntryJson.price;
+  }
+}
+
+export class ChartDataModel {
+  public data: Serie[];
+  constructor(data: Serie[]) {
+    this.data = data;
+  }
+}
+
+export class Serie {
+  public name: string;
+  public series: SerieEntry[];
+}
+
+export class SerieEntry {
+  public name: string;
+  public value: number;
+  constructor(price: PriceEntry) {
+    this.name = price.date;
+    this.value = price.price;
+  }
 }
