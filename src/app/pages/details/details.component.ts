fdec8aa35c5f3df0ac98281dd83bb463376dc99f
andream16 <andrea.medda.uni@gmail.com>
2017-12-17 16:34:58 +0100

    Refactoring Details to handle the chart.

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,67 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
 
+/** rxjs **/
+import { Observable } from 'rxjs/Observable';
+
 /** 3rd party **/
-import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { Store } from '@ngrx/store';
 
 /** ngrx **/
 import { AppState } from '../../shared/interfaces/state.interface';
-import { selectCurrentItem } from '../../shared/item/item.reducer';
+import {selectCurrentItem, selectCurrentChartData} from '../../shared/item/item.reducer';
 import * as itemActions from '../../shared/item/item.actions';
 
 /** App Models **/
-import { Item } from '../../shared/item/item.model';
-import { ChartDataModel } from './chart/chart.model';
+import {Item, ItemRequest} from '../../shared/item/item.model';
+import {ChartDataModel} from './price.model';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss'],
-  providers: [NgxChartsModule]
+  styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
 
-  item: Item;
+  item$: Observable<Item>;
+  chartData$: Observable<ChartDataModel>;
   // NGX
-  ngxData: ChartDataModel = {
-    data: [
-      {
-        name: 'prices',
-        series: []
-      }
-    ]
-  };
-  view: any[] = [1000, 600];
-  showXAxis = true;
-  showYAxis = true;
-  gradient = false;
-  showLegend = true;
-  showXAxisLabel = true;
+  view = [1000, 600];
   xAxisLabel = 'Dates';
-  showYAxisLabel = true;
   yAxisLabel = 'Prices';
   colorScheme = {
     domain: ['#f0ad4e', '#337ab7', '#C7B42C', '#AAAAAA']
   };
-  autoScale = true;
 
   constructor(private store: Store<AppState>, private route: ActivatedRoute) {
-    if ( this.item === undefined || this.item.id === undefined || this.item.id.length === 0 ) {
-      this.store.select(selectCurrentItem).subscribe((item) => {
-        if (item !== undefined && item.id !== undefined && item.id.length) {
-          this.item = item;
-        } else {
-          this.route.params.subscribe((params: Params) => {
-            if ( params !== undefined && params !== null && params.id !== undefined && params.id.length ) {
-              this.store.dispatch(new itemActions.LoadCurrentItemAction(
-                params.id
-              ));
-            }
-          });
-        }
-      });
-    }
+    this.item$ = this.store.select(selectCurrentItem);
+    this.chartData$ = this.store.select(selectCurrentChartData);
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      if ( params !== undefined && params !== null && params.id !== undefined && params.id.length ) {
+        this.store.dispatch(new itemActions.LoadCurrentItemAction(
+          params.id
+        ));
+        this.store.dispatch(new itemActions.LoadPricesByItemAction({item: params.id} as ItemRequest));
+      }
+    });
   }
 
   goToAmazon(item: Item) {
