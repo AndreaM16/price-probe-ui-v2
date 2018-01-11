8ac093881e4b03ed05459cf7cb54c1de95617469
andream16 <andrea.medda.uni@gmail.com>
2018-01-11 12:45:07 +0100

    Fixing HeaderBar to handle Item Research.

--- src/app/core/headerbar/headerbar.component.ts
+++ src/app/core/headerbar/headerbar.component.ts
@@ -1,6 +1,20 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+/** rxjs **/
+import { Subject } from 'rxjs/Subject';
+import { of } from 'rxjs/observable/of';
+
+/** 3rd party **/
+import { Store } from '@ngrx/store';
+
+/** ngrx **/
+import { AppState } from '../../shared/interfaces/state.interface';
+import * as ItemActions from '../../shared/item/item.actions';
+
+/** App Models **/
+import { ItemRequest } from '../../shared/item/item.model';
+
 @Component({
   selector: 'app-header-bar',
   templateUrl: './headerbar.component.html',
@@ -9,7 +23,16 @@ import { Router } from '@angular/router';
 
 export class HeaderbarComponent {
 
-  constructor(private router: Router) { }
+  searchInputChange$ = new Subject<string>();
+
+  constructor(private router: Router, private store: Store<AppState>) {
+    this.searchInputChange$
+      .switchMap((text) => of(text))
+      .subscribe(text => {
+        this.store.dispatch(new ItemActions.LoadCurrentItemAction({item : text} as ItemRequest));
+      })
+    ;
+  }
 
   goToHomePage() {
     this.router.navigate(['app/home']);
